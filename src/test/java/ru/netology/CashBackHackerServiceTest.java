fccfc0bc6dc6226d9e8749180b74023fef0b5283
Alina <alin4ik.grishina@gmail.com>
2025-11-06 13:05:06 +0700

    Editing commit testng

--- src/test/java/ru/netology/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/CashBackHackerServiceTest.java
@@ -1,4 +1,42 @@
 package ru.netology;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class CashBackHackerServiceTest {
+    @Test
+    public void amountEqualLimit() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void amountLessLimit() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void amountMoreLimit() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
 }
+
+
